a810fb7bf752ddde248d7c946b325920256948ed
Michael Chelen <michael.chelen@gmail.com>
2015-05-16 22:39:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+var text = "hello world";
+
+console.log(ABC.toBinary(text));
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("txt");
+    console.log(x);
+//    var text = "";
+//    var i;
+//    for (i = 0; i < x.length ;i++) {
+//        text += x.elements[i].value + "<br>";
+//    }
+    document.getElementById("demo").innerHTML = x;
+}
+
+
+
+
+function txt2dna(txt){
+
+//txt
+
+
+}
+
+
+
+
+
+

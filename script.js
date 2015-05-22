979e5415df98a6020a1ee25f2eaa3762b164af44
Mike Chelen <michael.chelen@gmail.com>
2015-05-21 21:31:52 -0400

    ascii and binary working

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 $( document ).ready(function() {
-       
-  $("#txt").keyup(function() {   
+  $("#txt").keyup(function() {
+  
+    $('#ascii').val(
+      $('#txt').val().split('').map(function(key){ 
+        return key.charCodeAt(0)
+      }).join(" ")
+    );
+     
+
     $('#binary').val(ABC.toBinary($('#txt').val()));
-  });    
     
+   
+    $('#ternary').val(    
+      $('#binary').val().split(" ").map(function(key){
+        
+        console.log(key);
+        
+        console.log(parseInt(key,2))
+        
+ //       return parseInt(key).toString(3);
+        
+        
+    }).join(" ")
+      
+    );
+  
+  
+  });      
 });
 
